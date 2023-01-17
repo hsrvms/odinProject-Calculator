b0131981360397a874d9ddd9125eafbce0d6b2f7
hsrvms <59848940+hsrvms@users.noreply.github.com>
2023-01-17 17:03:54 +0300

    add UI

---
+++ script.js
@@ -0,0 +1,34 @@
+
+
+const add = (a, b) => {
+  return a + b;
+}
+
+const subtract = (a, b) => {
+  return a - b;
+}
+
+const multiply = (a, b) => {
+  return a * b;
+}
+
+const divide = (a, b) => {
+  return a / b;
+}
+
+const operate = (operator, num1, num2) => {
+  switch (operator) {
+    case '+':
+      add(num1, num2);
+      break;
+    case '-':
+      subtract(num1, num2);
+      break;
+    case '*':
+      multiply(num1, num2);
+      break;
+    case '/':
+      divide(num1, num2);
+      break;
+  }
+}
